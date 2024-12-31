3f7705799a56e20efd553c8955e14ce9d74b9515
Abhigna Turaka <arunaturaka8074@gmail.com>
2024-12-31 11:37:50 +0530

    login regiter done

---
+++ main.js
@@ -0,0 +1,56 @@
+//Login Form Event Listener
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('LoginUserName').value;
+    const password = document.getElementById('LoginPass').value;
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const user = users.find((user) => user.username === email && user.password === password);
+    if(user) {
+        alert('Login Successful');
+        loginForm.reset();
+    }
+    else {
+        alert('Invalid email or password');
+    }
+}
+);
+
+
+const registrationForm = document.getElementById('regsiterForm');
+
+registrationForm.addEventListener('submit', (event) => { 
+    event.preventDefault();
+    const name = document.getElementById('registerName').value;
+    const username = document.getElementById('registerUserName').value;
+    const email = document.getElementById('registerEmail').value;
+    const password = document.getElementById('registerPass').value;
+    
+    const user = {
+        name,
+        username,
+        email,
+        password
+    };
+    
+    // if user email is exists in local storage then show alert
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const userExists = users.find((user) => user.email === email);
+    if(userExists) {
+        alert('User already exists with this email');
+        return;
+    }
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+
+    alert('User registered successfully');
+    registrationForm.reset();
+});
+
+
+
+
+
+    
